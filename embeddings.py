b04b6ee38ff2641c54ecc40715a019b683addb28
Denzell Ford <fdenzell@gmail.com>
2023-08-24 22:30:06 -0500

    feat: switched to fast api

--- embeddings.py
+++ embeddings.py
@@ -1,11 +1,13 @@
-# Load model directly
 from transformers import AutoTokenizer, AutoModel
+import uvicorn
 import torch
 import numpy as np
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
-import http.server
-import json
-
+# Create a Flask app
+app = FastAPI()
 
 if torch.cuda.is_available():
     # Initialize CUDA device
@@ -16,31 +18,20 @@ else:
 tokenizer = AutoTokenizer.from_pretrained("BAAI/bge-large-en")
 model = AutoModel.from_pretrained("BAAI/bge-large-en")
 model.to(device)
+# Tokenize sentences
 
-class JSONHandler(http.server.BaseHTTPRequestHandler):
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        try:
-            input_data = json.loads(post_data)
-            if 'input' in input_data and isinstance(input_data['input'], str):
-                response_data = encode(input_data['input'])
-                code = 200
-            else:
-                response_data = {"error": "Invalid input format", "status": 400}
-                code = 400
-        except json.JSONDecodeError:
-            response_data = {"error": "Invalid JSON", "status": 400}
-            code = 400
+@app.route("/")
+def health():
+    return "hello", 200
 
-        self.send_response(code)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(response_data).encode())
 
-def encode(data: str):
+class EncodeRequest(BaseModel):
+    input: str
+
+@app.post("/")
+async def encode(encodingRequest: EncodeRequest):
     encoded_input = tokenizer(
-        data,
+        encodingRequest.input,
         padding=True,
         truncation=True,
         max_length=512,
@@ -57,11 +48,9 @@ def encode(data: str):
         sentence_embeddings = model_output[0][:, 0]
     # normalize embeddings
     sentence_embeddings = torch.nn.functional.normalize(sentence_embeddings, p=2, dim=1)
-    return {"embeddings": np.array(sentence_embeddings.cpu())[0].tolist(), "status": 200}
+    return JSONResponse(content={"embeddings": np.array(sentence_embeddings.cpu())[0].tolist(), "status": 200})
 
-if __name__ == '__main__':
-    server_address = ('', 8080)  # Change the port as needed
-    httpd = http.server.HTTPServer(server_address, JSONHandler)
-    print('Server is running on port 8080')
-    httpd.serve_forever()
+
+if __name__ == "__main__":
+   uvicorn.run("embeddings:app", host="127.0.0.1", port=8080, reload=True)
 
