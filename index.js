0b566ffcc3fd65fd2423e52340cd6cc41a9c8b47
Denzell Ford <fdenzell@gmail.com>
2023-08-24 22:30:16 -0500

    feat: switched to express js

--- index.js
+++ index.js
@@ -17,118 +17,12 @@ if (process.env.SALT === undefined) {
 }
 const salt = process.env.SALT || crypto.randomBytes(32).toString('base64');
 
-// JSON schema validation using Zod
-const encodeInputSchema = object({
-  input: string(),
-  key: string(),
-});
-
 const validKey = (key) => {
   if (key.startsWith("EMB-") && key.length == 36) {
     return true;
   }
   return false;
 }
-
-// Create a basic HTTP server
-const server = http.createServer((req, res) => {
-  const { pathname, query } = url.parse(req.url, true);
-  console.log(pathname, query);
-  if (pathname === '/encode') {
-    if (req.method !== 'POST') {
-      res.writeHead(405, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ error: 'Method not allowed' }));
-    }
-
-    let data = '';
-
-    req.on('data', chunk => {
-      data += chunk;
-    });
-
-    req.on('end', () => {
-      try {
-        const parsedData = JSON.parse(data);
-        const validatedData = encodeInputSchema.parse(parsedData);
-
-        const tokens = (ethers.toBigInt(validatedData.input.length) / ethers.toBigInt(4)) + ethers.toBigInt(1);
-        const amount = tokens * ETH_TOK_EXCHANGE_RATE;
-        monterrey.debit(validatedData.key, amount).then((success) => {
-          if (!success) {
-            res.writeHead(400, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ error: 'Insufficient funds' }));
-            return;
-          }
-          fetch(`${EMBEDDING_SERVICE_URL}`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ input: validatedData.input }),
-          })
-            .then(res => res.json())
-            .then(data => {
-              if (data.status === 200) {
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify(data));
-              } else {
-                res.writeHead(400, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ error: data }));
-              }
-            });
-        })
-
-      } catch (error) {
-        if (error instanceof ZodError) {
-          res.writeHead(400, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify({ "error": error.issues }));
-        } else {
-          console.log("Error", error);
-          res.writeHead(500, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify({ error: error }));
-        }
-      }
-    });
-  } else if (req.method == "GET" && req.url.startsWith("/gateway/")) {
-    const key = req.url.split('/gateway/')[1];
-    if (!validKey(key)) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ error: 'Invalid key' }));
-      return;
-    }
-    monterrey.generate(key).then(wallet => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify({ address: wallet.address }));
-    });
-  } else if (req.method == "GET" && pathname == "/key") {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ key: "EMB-" + hat() }));
-  } else if (req.method == "GET" && req.url.startsWith("/balance/")) {
-    const key = req.url.split('/balance/')[1];
-    if (key && !validKey(key)) {
-      console.log(key.length, key.startsWith("EMB-"));
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ error: 'Invalid key' }));
-      return;
-    }
-    return monterrey.getBalance(key).then(balance => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify({ balance: ethers.formatEther(balance) }));
-    }).catch(err => {
-      console.log(err)
-      res.statusCode = 500;
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify({ error: err }));
-    });
-
-  } else {
-    res.writeHead(404, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ error: 'Route not found' }));
-  }
-});
 
 let monterrey = null;
 Monterrey.create({
@@ -153,6 +47,82 @@ Monterrey.create({
 
 // Start the server
 const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+
+const express = require('express')
+const app = express()
+
+function asyncHandler(callback) {
+  return function(req, res, next) {
+    callback(req, res, next)
+      .catch(next)
+  }
+}
+
+app.get('/', asyncHandler(async (_, res) => {
+  res.send('Hello World!')
+}))
+
+app.get('/balance/:key', asyncHandler(async (req, res) => {
+  const key = req.params.key;
+
+  const balance = await monterrey.getBalance(key);
+  return res.json({ balance: balance });
+}))
+
+app.get('/key', asyncHandler(async (_, res) => {
+  return res.json({ key: "EMB-" + hat() });
+}))
+
+app.get('/gateway/:key', asyncHandler(async (req, res) => {
+  const key = req.params.key;
+
+  const wallet = await monterrey.generate(key);
+  return res.json({ address: wallet.address });
+}))
+
+
+const encodeInputSchema = object({
+  input: string(),
+  key: string(),
 });
+
+app.post('/encode', asyncHandler(async (req, res) => {
+  let data;
+  try {
+    data = await encodeInputSchema.parse(req.body);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      res.json({ "error": error.issues });
+    } else {
+      throw error;
+    }
+  }
+  const tokens = (ethers.toBigInt(data.input.length) / ethers.toBigInt(4)) + ethers.toBigInt(1);
+  if (!validKey(data.key)) {
+    res.json({ error: 'Invalid key' });
+    return;
+  }
+
+  const success = await monterrey.debit(data.key, tokens)
+  if (!success) {
+    res.status(400).json({ error: 'Insufficient funds' });
+    return;
+  }
+
+  const embeddingResponse = await fetch(`${EMBEDDING_SERVICE_URL}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ input: validatedData.input }),
+  })
+
+  const json = embeddingResponse.json();
+  return res.json(json);
+
+}));
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
+
