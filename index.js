8d9eed36cff9b951c8a9ed5418dd46af30ff828d
Denzell Ford <fdenzell@gmail.com>
2023-08-25 07:33:45 -0500

    bug and formating

--- index.js
+++ index.js
@@ -14,36 +14,36 @@ const ETH_TOK_EXCHANGE_RATE = ethers.toBigInt(process.env.ETH_TOK_EXCHANGE_RATE 
 const EMBEDDING_SERVICE_URL = process.env.EMBEDDING_SERVICE_URL;
 const RPC_URL = process.env.RPC_URL;
 if (process.env.SALT === undefined) {
-  console.warn('No SALT environment variable found, generating a random one');
+	console.warn('No SALT environment variable found, generating a random one');
 }
 const salt = process.env.SALT || crypto.randomBytes(32).toString('base64');
 
 const validKey = (key) => {
-  if (key.startsWith("EMB-") && key.length == 36) {
-    return true;
-  }
-  return false;
+	if (key.startsWith("EMB-") && key.length == 36) {
+		return true;
+	}
+	return false;
 }
 
 let monterrey = null;
 Monterrey.create({
-  backend: path.join(process.env.HOME, '.embeddings'),
-  salt,
-  provider: RPC_URL? new ethers.JsonRpcProvider(RPC_URL): null,
-  ethConversion: ETH_TOK_EXCHANGE_RATE,
-  tokenConversionRate: {
-    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': 6666666
-  }
+	backend: path.join(process.env.HOME, '.embeddings'),
+	salt,
+	provider: RPC_URL ? new ethers.JsonRpcProvider(RPC_URL) : null,
+	ethConversion: ETH_TOK_EXCHANGE_RATE,
+	tokenConversionRate: {
+		'0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': 6666666
+	}
 }).then(m => {
-  monterrey = m;
-  monterrey.start(); // return value is an unsubscribe function to stop monterrey
+	monterrey = m;
+	monterrey.start(); // return value is an unsubscribe function to stop monterrey
 
-  monterrey.on('credit', ({ account, amount }) => {
-    console.log('user ' + account + ' balance increases by ' + ethers.formatEther(amount));
-  });
-  monterrey.on('debit', ({ account, amount }) => {
-    console.log('user ' + account + ' balance decreases by ' + ethers.formatEther(amount));
-  });
+	monterrey.on('credit', ({ account, amount }) => {
+		console.log('user ' + account + ' balance increases by ' + ethers.formatEther(amount));
+	});
+	monterrey.on('debit', ({ account, amount }) => {
+		console.log('user ' + account + ' balance decreases by ' + ethers.formatEther(amount));
+	});
 });
 
 // Start the server
@@ -55,97 +55,97 @@ const app = express()
 app.use(express.json());
 
 function asyncHandler(callback) {
-  return function(req, res, next) {
-    callback(req, res, next)
-      .catch(next)
-  }
+	return function(req, res, next) {
+		callback(req, res, next)
+			.catch(next)
+	}
 }
 
 app.get('/', asyncHandler(async (_, res) => {
-  res.send('Hello World!')
+	res.send('Hello World!')
 }))
 
 app.get('/balance/:key', asyncHandler(async (req, res) => {
-  const key = req.params.key;
-  if (!validKey(key)) {
-    res.status(400).send('Invalid key');
-    return;
-  }
+	const key = req.params.key;
+	if (!validKey(key)) {
+		res.status(400).send('Invalid key');
+		return;
+	}
 
-  const balance = await monterrey.getBalance(key);
-  return res.json({ balance: ethers.formatEther(balance) });
+	const balance = await monterrey.getBalance(key);
+	return res.json({ balance: ethers.formatEther(balance) });
 }))
 
 app.get('/key', asyncHandler(async (_, res) => {
-  return res.json({ key: "EMB-" + hat() });
+	return res.json({ key: "EMB-" + hat() });
 }))
 
 app.get('/gateway/:key', asyncHandler(async (req, res) => {
-  const key = req.params.key;
-  if (!validKey(key)) {
-    res.status(400).send('Invalid key');
-    return;
-  }
+	const key = req.params.key;
+	if (!validKey(key)) {
+		res.status(400).send('Invalid key');
+		return;
+	}
 
-  const wallet = await monterrey.generate(key);
-  return res.json({ address: wallet.address });
+	const wallet = await monterrey.generate(key);
+	return res.json({ address: wallet.address });
 }))
 
 
 const encodeInputSchema = object({
-  input: string(),
-  key: string(),
-  model: string(),
+	input: string(),
+	key: string(),
+	model: string(),
 });
 
 app.post('/encode', asyncHandler(async (req, res) => {
-  let data;
-  try {
-    data = encodeInputSchema.parse(req.body);
-  } catch (error) {
-    if (error instanceof ZodError) {
-      res.status(400).json({ "error": error.issues });
-    } else {
-      throw error;
-    }
-  }
-  const tokens = (ethers.toBigInt(data.input.length) / ethers.toBigInt(4)) + ethers.toBigInt(1);
-  if (!validKey(data.key)) {
-    res.json({ error: 'Invalid key' });
-    return;
-  }
-
-  const success = await monterrey.debit(data.key, tokens)
-  if (!success) {
-    res.status(400).json({ error: 'Insufficient funds' });
-    return;
-  }
-
-  let embeddingResponse;
-  try { 
-   embeddingResponse = await fetch(`${EMBEDDING_SERVICE_URL}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ input: data.input }),
-  })
-  } catch (e) {
-		console.log(e);
-    monterrey.credit(data.key, tokens);
+	let data;
+	try {
+		data = encodeInputSchema.parse(req.body);
+	} catch (error) {
+		if (error instanceof ZodError) {
+			res.status(400).json({ "error": error.issues });
+		} else {
+			throw error;
+		}
 	}
-  if (embeddingResponse.status !== 200) {
-    res.status(400).json({ error: 'Embedding service error' });
-    monterrey.credit(data.key, tokens);
-    return;
-  }
+	const tokens = (ethers.toBigInt(data.input.length) / ethers.toBigInt(4)) + ethers.toBigInt(1);
+	if (!validKey(data.key)) {
+		res.json({ error: 'Invalid key' });
+		return;
+	}
 
-  const json = embeddingResponse.json();
-  return res.json(json);
+	const success = await monterrey.debit(data.key, tokens)
+	if (!success) {
+		res.status(400).json({ error: 'Insufficient funds' });
+		return;
+	}
+
+	let embeddingResponse;
+	try {
+		embeddingResponse = await fetch(`${EMBEDDING_SERVICE_URL}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ input: data.input }),
+		})
+	} catch (e) {
+		console.log(e);
+		monterrey.credit(data.key, tokens);
+	}
+
+	if (embeddingResponse !== 200) {
+		monterrey.credit(data.key, tokens);
+		return res.status(400).json({ error: 'Embedding service error' });;
+	}
+
+	const json = embeddingResponse.json();
+	return res.json(json);
 
 }));
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
+	console.log(`Listening on port ${PORT}`)
 })
 
